28cb700d2b2d99d39f787b537ae702a88cf93092
Regina Furness <skritschy@gmail.com>
2020-04-22 19:26:00 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,11 +6,12 @@ class CashRegister
   end 
 
   def add_item(item_name,item_price, quantity = 1)
+    @item_price = item_price
     @quantity = quantity
     @total = @total + item_price * @quantity
   end 
   
   def apply_discount
-    @total = @total - ((item_price * discount)%100)
+    @total = @total - ((@item_price * discount)%100)
   end 
 end 
