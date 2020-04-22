fa88489c50de3419e0463c4b8a52d770e4df96b1
Regina Furness <skritschy@gmail.com>
2020-04-22 19:32:16 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,6 +16,7 @@ class CashRegister
   def apply_discount
     if discount > 0
     @total = @total - ((@discount * @item_price)/100)
+    "After the discount, the total comes to #{@total}."
    end
   end 
 end 
